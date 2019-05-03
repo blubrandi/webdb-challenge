66cfacb98d7e883aa2915db14bc0ca9d4d33748c
blubrandi <blubrandi0@gmail.com>
2019-05-03 10:23:38 -0600

    configured projects migration file.

--- data/migrations/20190503095139_projects.js
+++ data/migrations/20190503095139_projects.js
@@ -1,8 +1,20 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments()
+
+            tbl
+                .string('name', 128)
+                .string('desc', 256)
+                .boolean('completed')
+                .notNullable()
+                .unique()
+        })
+
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema
+        .dropTableIfExists('projects')
 };
