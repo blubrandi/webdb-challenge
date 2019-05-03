9077af262416c033a3845c0932f2c82fa158cb9f
blubrandi <blubrandi0@gmail.com>
2019-05-03 13:41:42 -0600

    Completed MVP and stretch with seeding

--- index.js
+++ index.js
@@ -28,26 +28,86 @@ server.get('/api/projects', async (req, res) => {
     }
 });
 
+server.get('/api/projects/:id', async (req, res) => {
+    try {
+        const projects = await db('projects').where({ id: req.params.id });
+        const actions = await db('actions').where({ project_id: req.params.id });
+        if (projects.length) {
+            const project = projects[0];
+            res.status(200).json({ ...project, actions });
+        } else {
+            res
+                .status(404)
+                .json({ message: 'Project cannot be found' });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'There was an error retrieving your project',
+            error
+        });
+    }
+});
+
 
 const errors = {
     '19': 'Another record with that value exists',
 };
 
-// create roles
 server.post('/api/projects', async (req, res) => {
+    if (!req.body.name || !req.body.description) {
+        return res.status(400).json({
+            message:
+                'Please add a project name and description'
+        });
+    }
     try {
-        const [id] = await db('projects').insert(req.body);
-
-        const role = await db('projects')
-            .where({ id })
-            .first();
-
-        res.status(201).json(role);
+        const project = await db('projects').insert(req.body);
+        if (project) {
+            res
+                .status(200)
+                .json({ message: 'Project created successfully.', project });
+        } else {
+            res
+                .status(404)
+                .json({ message: 'We could not add your project' });
+        }
     } catch (error) {
-        const message = errors[error.errno] || 'We ran into an error';
-        res.status(500).json({ message, error });
+        res.status(500).json({
+            message: 'We could not add your project',
+            error
+        });
     }
 });
+
+server.post('/api/actions', async (req, res) => {
+    if (
+        !req.body.description ||
+        !req.body.notes ||
+        !req.body.project_id
+    ) {
+        return res.status(400).json({
+            message:
+                'Please provide all necessary info'
+        });
+    }
+    try {
+        const action = await db('actions').insert(req.body);
+        if (action) {
+            res.status(200).json({
+                message: 'Action created',
+            });
+        } else {
+            res
+                .status(404)
+                .json({ message: 'This action cannot be added.' });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'There was an error adding your action.',
+            error
+        });
+    }
+})
 
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
